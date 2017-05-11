c9d18ea4b04ca2a6e9e5b773d27f51fc586acd11
marcecabelloherrera <marcecabello.h@gmail.com>
2017-05-11 18:42:03 -0300

    correciones listas, ejercicio guiado finalizado

--- archivo.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("toogle").addEventListener("click", function(event){
-	event.preventDefault();
-	document.getElementById("nav-header").classList.toogle("open");
-	document.getElementById("body").classList.toogle("overflow-hidden");
-
-});
